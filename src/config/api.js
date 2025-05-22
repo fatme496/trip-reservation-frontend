c84be16ad00e3c9b6ba68ec041ededd778dfdf11
Fatme <fatme34saeed@gmail.com>
2025-05-22 12:41:58 +0300

    Updating on the config

--- src/config/api.js
+++ src/config/api.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://trip-reservation-backend.onrender.com" || "http://localhost:5000";
+export const API_URL = import.meta.env.REACT_APP_API_URL || "http://localhost:5000";
