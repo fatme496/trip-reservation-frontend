4704e5094c79ff050f74baab9a780a0b72e02d55
Fatme <fatme34saeed@gmail.com>
2025-05-27 07:42:29 +0300

    Redeploy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import TripCard from '../components/TripCard.js';
+// import TripCard from '../components/TripCard.js';
 // import './HomePage.css'; // Optional: Add any additional styling
-import TripList from '../components/TripList.js';
+// import TripList from '../components/TripList.js';
 import HeroSection from '../components/HeroSection.js';
 import WhyTripInLebSection from '../components/WhyTripInLebSection.js';
 import HowItWorksSection from '../components/HowItWorksSection.js';
